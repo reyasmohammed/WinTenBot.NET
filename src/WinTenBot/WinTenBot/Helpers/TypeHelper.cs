02b65c9858c3298829f40ed3ac27f09f6224addd
Azhe Kun <azhe403@gmail.com>
2019-12-08 22:33:01 +0700

    add ToBool in type helper

---
+++ src/WinTenBot/WinTenBot/Helpers/TypeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinTenBot.Helpers
+{
+    public static class TypeHelper
+    {
+        public static bool ToBool(this object obj)
+        {
+//            return bool.Parse(obj);
+            return Convert.ToBoolean(obj);
+        }
+    }
+}
