978e9481c3c95a7ba141d7cb7fb703bfec290571
Azhe Kun <azhe403@gmail.com>
2019-12-08 22:37:27 +0700

    add update cache settings

--- src/WinTenBot/WinTenBot/Services/SettingsService.cs
+++ src/WinTenBot/WinTenBot/Services/SettingsService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Threading.Tasks;
+using WinTenBot.Helpers;
 using WinTenBot.Providers;
 
 namespace WinTenBot.Services
@@ -7,12 +8,14 @@ namespace WinTenBot.Services
     public class SettingsService
     {
         private readonly MySqlProvider _mySqlProvider;
+        public long ChatId { get; set; }
+        
         public SettingsService()
         {
             _mySqlProvider = new MySqlProvider();
         }
         
-        public async Task<DataTable> GetSettingByGrup(long chatId)
+        public async Task<DataTable> GetSettingByGroup(long chatId)
         {
             var sql = $"SELECT * FROM group_settings WHERE chat_id = '{chatId}'";
             var data = await _mySqlProvider.ExecQueryAsync(sql);
@@ -26,6 +29,12 @@ namespace WinTenBot.Services
                       $"WHERE chat_id = '{chatId}'";
 
             await _mySqlProvider.ExecNonQueryAsync(sql);
+        }
+
+        public async Task UpdateCache()
+        {
+            var data = await GetSettingByGroup(ChatId);
+            await data.WriteCacheAsync($"{ChatId}/settings.json");
         }
     }
 }
