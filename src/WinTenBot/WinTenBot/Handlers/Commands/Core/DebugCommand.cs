816952837ed2bbc5dfe3e9d96af5fa30bb63f0d4
Azhe Kun <azhe403@gmail.com>
2019-12-08 22:35:47 +0700

    add debug command

---
+++ src/WinTenBot/WinTenBot/Handlers/Commands/Core/DebugCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Framework.Abstractions;
+using WinTenBot.Helpers;
+using WinTenBot.Helpers.Processors;
+
+namespace WinTenBot.Handlers.Commands.Core
+{
+    public class DebugCommand:CommandBase
+    {
+        private ChatProcessor _chatProcessor;
+        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args, CancellationToken cancellationToken)
+        {
+            _chatProcessor = new ChatProcessor(context);
+
+            var msg = context.Update.Message;
+            var json = msg.ToJson(true);
+            
+            ConsoleHelper.WriteLine(json.Length);
+
+            var sendText = $"Debug:\n {json}";
+            await _chatProcessor.SendAsync(sendText);
+        }
+    }
+}
