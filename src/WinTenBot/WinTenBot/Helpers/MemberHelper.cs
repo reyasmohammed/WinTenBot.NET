6a1930aa2946d3d33b7650eedd7bec4c65d4df51
Azhe Kun <azhe403@gmail.com>
2019-12-08 22:30:35 +0700

    get name link from message

--- src/WinTenBot/WinTenBot/Helpers/MemberHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/MemberHelper.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace WinTenBot.Helpers
 {
-    public class MemberHelper
+    public static class MemberHelper
     {
         public static string GetNameLink(int userId, string Name)
         {
             return $"<a href='tg://user?id={userId}'>{Name}</a>";
+        }
+        
+        public static string GetFromNameLink(this Message message)
+        {
+            var firstName = message.From.FirstName;
+            var lastName = message.From.LastName;
+            
+            return $"<a href='tg://user?id={message.From.Id}'>{(firstName + " " + lastName).Trim()}</a>";
+        }
+
+        public static async Task<bool> IsAdminGroup(ITelegramBotClient client, Message message)
+        {
+            var chatId = message.Chat.Id;
+            var userId = message.From.Id;
+            var isAdmin = false;
+
+            var admins = await client.GetChatAdministratorsAsync(chatId);
+            foreach (var admin in admins)
+            {
+                if (userId == admin.User.Id)
+                {
+                    isAdmin = true;
+                }
+            }
+
+            return isAdmin;
         }
     }
 }
