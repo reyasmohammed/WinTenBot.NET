8ae33c44323d5d5a8f92db412b8ae9f3207da540
Azhe Kun <azhe403@gmail.com>
2019-12-10 22:24:36 +0700

    disable cas ban when run on local

--- src/WinTenBot/WinTenBot/Handlers/GenericMessageHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/GenericMessageHandler.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types;
 using WinTenBot.Helpers;
+using WinTenBot.Helpers.Processors;
+using WinTenBot.Model;
 using WinTenBot.Providers;
 
 namespace WinTenBot.Handlers
@@ -11,18 +15,26 @@ namespace WinTenBot.Handlers
     public class GenericMessageHandler : IUpdateHandler
     {
         private CasBanProvider _casBanProvider;
+        private ChatProcessor _chatProcessor;
 
         public GenericMessageHandler()
         {
             _casBanProvider = new CasBanProvider();
         }
+
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
+            _chatProcessor = new ChatProcessor(context);
             Message msg = context.Update.Message;
+
             ConsoleHelper.WriteLine(msg.ToJson());
-            
-            await _casBanProvider.IsCasBan(msg.From.Id);
-            
+            ConsoleHelper.WriteLine(msg.Text);
+
+            if (Bot.HostingEnvironment.IsProduction())
+            {
+                await _casBanProvider.IsCasBan(msg.From.Id);
+            }
+
             //            await context.Bot.Client.SendTextMessageAsync(
             //                msg.Chat, "You said:\n" + msg.Text
             //            );
