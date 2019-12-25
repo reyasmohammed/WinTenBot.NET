c27871d9e6dc935f0ae86b88a2c673f14809f370
Azhe Kun <azhe403@gmail.com>
2019-12-26 00:03:33 +0700

    add class NewMember model

---
+++ src/WinTenBot/WinTenBot/Model/NewMember.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace WinTenBot.Model
+{
+    public class NewMember
+    {
+        public StringBuilder AllNewMember { get; set; }
+        public StringBuilder AllNoUsername { get; set; }
+        public StringBuilder AllNewBot { get; set; }
+        
+        // public string[] AllNewMember { get; set; }
+        // public string[] AllNoUsername { get; set; }
+        // public string[] AllNewBot { get; set; }
+    }
+}
