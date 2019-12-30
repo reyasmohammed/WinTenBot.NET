1a76025f4bd9775494519cd0f8bebe6d355b4a77
Azhe Kun <azhe403@gmail.com>
2019-12-30 21:14:36 +0700

    sticker reply match group sticker set

--- src/WinTenBot/WinTenBot/Handlers/StickerHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/StickerHandler.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types;
+using WinTenBot.Helpers;
 
 namespace WinTenBot.Handlers
 {
@@ -10,10 +11,14 @@ namespace WinTenBot.Handlers
     {
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
+            ChatHelper.Init(context);
+            
             Message msg = context.Update.Message;
             Sticker incomingSticker = msg.Sticker;
 
-            StickerSet evilMindsSet = await context.Bot.Client.GetStickerSetAsync("EvilMinds");
+            var chat = await ChatHelper.GetChat();
+            var stickerSetName = chat.StickerSetName ?? "EvilMinds";
+            StickerSet evilMindsSet = await context.Bot.Client.GetStickerSetAsync(stickerSetName, cancellationToken);
 
             Sticker similarEvilMindSticker = evilMindsSet.Stickers.FirstOrDefault(
                 sticker => incomingSticker.Emoji.Contains(sticker.Emoji)
@@ -24,8 +29,7 @@ namespace WinTenBot.Handlers
             await context.Bot.Client.SendStickerAsync(
                 msg.Chat,
                 replySticker.FileId,
-                replyToMessageId: msg.MessageId
-            );
+                replyToMessageId: msg.MessageId, cancellationToken: cancellationToken);
         }
     }
 }
