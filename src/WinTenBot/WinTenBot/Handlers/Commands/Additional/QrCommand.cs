17760e18da648c6d5a4b0ffc84c583313bc260db
Azhe Kun <azhe403@gmail.com>
2020-01-01 18:57:01 +0700

    add button for qr source text

--- src/WinTenBot/WinTenBot/Handlers/Commands/Additional/QrCommand.cs
+++ src/WinTenBot/WinTenBot/Handlers/Commands/Additional/QrCommand.cs
@@ -1,37 +1,54 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Framework.Abstractions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 using WinTenBot.Helpers;
 using WinTenBot.Helpers.Processors;
 
 namespace WinTenBot.Handlers.Commands.Additional
 {
-    public class QrCommand:CommandBase
+    public class QrCommand : CommandBase
     {
         private ChatProcessor _chatProcessor;
-        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args, CancellationToken cancellationToken)
+
+        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
+            CancellationToken cancellationToken)
         {
             _chatProcessor = new ChatProcessor(context);
             var msg = context.Update.Message;
+            Message repMsg = null;
             var data = msg.Text.GetTextWithoutCmd();
-            
+
             if (msg.ReplyToMessage != null)
             {
-                data = msg.ReplyToMessage.Text?? msg.ReplyToMessage.Caption;
-
+                repMsg = msg.ReplyToMessage;
+                data = repMsg.Text ?? repMsg.Caption;
             }
 
             if (data == "")
             {
                 var sendText = "<b>Generate QR from text or caption media</b>" +
-                        "\n<b>Usage : </b><code>/qr</code> (In-Reply)" +
-                        "\n                <code>/qr your text here</code> (In-Message)";
+                               "\n<b>Usage : </b><code>/qr</code> (In-Reply)" +
+                               "\n                <code>/qr your text here</code> (In-Message)";
                 await _chatProcessor.SendAsync(sendText);
                 return;
             }
 
+            InlineKeyboardMarkup keyboard = null;
+            if (repMsg != null)
+            {
+                keyboard = new InlineKeyboardMarkup(
+                    InlineKeyboardButton.WithUrl("Sumber", repMsg.GetMessageLink())
+                );
+            }
+
             var urlQr = data.GenerateUrlQrApi();
-            await _chatProcessor.SendMediaAsync(urlQr,"photo");
+            var fileName = $"{msg.Chat.Id}_{msg.MessageId}.jpg";
+            
+            // urlQr.SaveUrlTo(fileName);
+            await _chatProcessor.SendMediaAsync(urlQr, "photo", replyMarkup: keyboard);
         }
     }
 }
