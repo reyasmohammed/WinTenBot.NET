f29a295705f6e48d89235a0b9016ee8d43245986
Azhe Kun <azhe403@gmail.com>
2020-01-11 19:27:44 +0700

    rename old Query builder prevent conflick

--- src/WinTenBot/WinTenBot/Providers/QueryEx.cs
+++ src/WinTenBot/WinTenBot/Providers/QueryEx.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using WinTenBot.Helpers;
-using WinTenBot.Providers;
 
-namespace WinTenBot.Services
+namespace WinTenBot.Providers
 {
-    public class Query
+    public class QueryEx
     {
         private readonly MySqlProvider _dbConn = new MySqlProvider();
 
@@ -55,7 +53,7 @@ namespace WinTenBot.Services
             return kode;
         }
 
-        public async Task<DataTable> Select(string tabel, string cari)
+        public async Task<DataTable> Select(string tabel, object cari)
         {
             var query1 = "SELECT GROUP_CONCAT(column_name ORDER BY ordinal_position) as concats" +
                          " FROM information_schema.columns" +
@@ -66,12 +64,14 @@ namespace WinTenBot.Services
             {
                 var concats = dTbl1.Rows[0];
 
-                var query2 = "SELECT * FROM " + tabel + " WHERE concat(" + concats["concats"] + ") like '%" + cari + "%'";
+                var query2 = "SELECT * FROM " + tabel + " WHERE concat(" + concats["concats"] + ") like '%" + cari +
+                             "%'";
                 var dTbl2 = await _dbConn.ExecQueryAsync(query2);
 
                 if (dTbl2.Rows.Count > 0)
                     return dTbl2;
             }
+
             return new DataTable(null);
         }
 
@@ -122,7 +122,7 @@ namespace WinTenBot.Services
 
             var query = $"UPDATE {tabel} SET {kolom} WHERE {where.First().Key} = \'{where.First().Value}\'";
             var result = await _dbConn.ExecNonQueryAsync(query);
-            if ( result> 0) update = true;
+            if (result > 0) update = true;
 
             return update;
         }
@@ -133,7 +133,7 @@ namespace WinTenBot.Services
 
             var query = $"DELETE FROM {tabel} WHERE {where.First().Key} = \'{where.First().Value}\'";
             var result = await _dbConn.ExecNonQueryAsync(query);
-            if ( result> 0) hapus = true;
+            if (result > 0) hapus = true;
 
             return hapus;
         }
@@ -150,9 +150,8 @@ namespace WinTenBot.Services
         public async Task<bool> ExeQuery(string query)
         {
             var result = await _dbConn.ExecNonQueryAsync(query);
-            if ( result> 0) return true;
+            if (result > 0) return true;
             return false;
         }
-        
     }
 }
