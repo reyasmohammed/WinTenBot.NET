46d026d855ab05d7e1e26c78eac0f2dcb35ca976
Azhe Kun <azhe403@gmail.com>
2020-01-18 12:27:46 +0700

    rsspull in background

--- src/WinTenBot/WinTenBot/Handlers/Commands/Rss/RssPullCommand.cs
+++ src/WinTenBot/WinTenBot/Handlers/Commands/Rss/RssPullCommand.cs
@@ -1,31 +1,45 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Extensions.Hosting.Internal;
 using Telegram.Bot.Framework.Abstractions;
 using WinTenBot.Helpers;
+using WinTenBot.Providers;
 
 namespace WinTenBot.Handlers.Commands.Rss
 {
-    public class RssPullCommand:CommandBase
+    public class RssPullCommand : CommandBase
     {
-        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args, CancellationToken cancellationToken)
-        { 
+        // private ResponseProvider response;
+        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
+            CancellationToken cancellationToken)
+        {
             ChatHelper.Init(context);
-            
+            // response = new ResponseProvider(context);
+
             var chatId = ChatHelper.Message.Chat.Id.ToString();
             var isAdmin = await ChatHelper.IsAdminGroup();
 
             if (isAdmin || ChatHelper.IsPrivateChat())
             {
-                await "Sedang memeriksa RSS feed baru..".SendTextAsync();
-                
-                var newRssCount = await RssHelper.ExecBroadcasterAsync(chatId);
-                if (newRssCount == 0)
+                Task.Run(async () =>
                 {
-                    await "Tampaknya tidak ada RSS baru saat ini".EditAsync();
-                }
+                    Thread.CurrentThread.IsBackground = true;
+
+                    // await "Sedang memeriksa RSS feed baru..".SendTextAsync();
+                    await ChatHelper.SendTextAsync("Sedang memeriksa RSS feed baru..");
+
+                    var newRssCount = RssHelper.ExecBroadcasterAsync(chatId).Result;
+                    if (newRssCount == 0)
+                    {
+                        await ChatHelper.EditAsync("Tampaknya tidak ada RSS baru saat ini");
+                    }
+                    
+                    ChatHelper.Close();
+                    
+                }, cancellationToken);
             }
-            
-            ChatHelper.Close();
+
         }
     }
 }
