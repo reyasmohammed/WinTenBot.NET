77bbe140eddd8417707314a05ce93715befd1b16
Azhe Kun <azhe403@gmail.com>
2020-01-18 16:47:17 +0700

    without await

--- src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
+using Serilog;
 using SqlKata;
 using SqlKata.Execution;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types;
 using WinTenBot.Helpers;
-using WinTenBot.Model;
 using WinTenBot.Providers;
 using WinTenBot.Services;
 
@@ -27,23 +25,24 @@ namespace WinTenBot.Handlers
         {
             ChatHelper.Init(context);
 
-            ConsoleHelper.WriteLine("New Update");
-            if (EnvironmentHelper.IsDev())
-            {
-                context.ToJson().ToConsoleStamp();
-            }
+            Log.Information("New Update");
+            // if (EnvironmentHelper.IsDev())
+            // {
+            //     context.ToJson().ToConsoleStamp();
+            // }
 
             var message = context.Update.Message ?? context.Update.CallbackQuery.Message;
 
-            await AfkCheck(message);
-            await CheckUsername(message);
+#pragma warning disable 4014
+
+            AfkCheck(message);
+            CheckUsername(message);
 
             if (!ChatHelper.IsPrivateChat())
             {
-                await CheckGlobalBanAsync(message);
+                CheckGlobalBanAsync(message);
             }
 
-#pragma warning disable 4014
             HitActivity(message);
 #pragma warning restore 4014
 
@@ -111,21 +110,21 @@ namespace WinTenBot.Handlers
 
         private async Task HitActivity(Message message)
         {
-            var data = new Dictionary<string,object>()
+            var data = new Dictionary<string, object>()
             {
-                {"via_bot","ZiziBeta"},
-                {"message_type",message.Type.ToString()},
-                {"from_id",message.From.Id},
-                {"from_first_name",message.From.FirstName},
-                {"from_last_name",message.From.LastName},
-                {"from_username",message.From.Username},
-                {"from_lang_code",message.From.LanguageCode},
-                {"chat_id",message.Chat.Id},
-                {"chat_username",message.Chat.Username},
-                {"chat_type",message.Chat.Type.ToString()},
-                {"chat_title",message.Chat.Title},
+                {"via_bot", "ZiziBeta"},
+                {"message_type", message.Type.ToString()},
+                {"from_id", message.From.Id},
+                {"from_first_name", message.From.FirstName},
+                {"from_last_name", message.From.LastName},
+                {"from_username", message.From.Username},
+                {"from_lang_code", message.From.LanguageCode},
+                {"chat_id", message.Chat.Id},
+                {"chat_username", message.Chat.Username},
+                {"chat_type", message.Chat.Type.ToString()},
+                {"chat_title", message.Chat.Title},
             };
-            
+
             var insertHit = await new Query("hit_activity")
                 .ExecForMysql()
                 .InsertAsync(data);
