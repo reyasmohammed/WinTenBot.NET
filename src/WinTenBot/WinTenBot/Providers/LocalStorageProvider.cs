bbc032f26f26a19405f47e6559e86599489f402b
Azhe Kun <azhe403@gmail.com>
2020-01-18 16:13:00 +0700

    add sqlite as Local Storage

---
+++ src/WinTenBot/WinTenBot/Providers/LocalStorageProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using Serilog;
+using SqlKata;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+
+namespace WinTenBot.Providers
+{
+    public static class LocalStorageProvider
+    {
+        static string dbPath = "Storage/Common/LocalStorage.db";
+
+        private static SQLiteConnection InitSqLite()
+        {
+            if (File.Exists(dbPath)) return new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            
+            Log.Information($"Creating {dbPath} for LocalStorage");
+            SQLiteConnection.CreateFile(dbPath);
+
+            return new SQLiteConnection($"Data Source={dbPath};Version=3;");
+        }
+        
+        public static Query ExecForSqLite(this Query query, bool printSql = false)
+        {
+            // var connection = new SQLiteConnection(dbPath);
+            var connection = InitSqLite();
+            
+            var factory = new QueryFactory(connection, new SqliteCompiler());
+
+            if (printSql) factory.Logger = sqlResult => { Log.Debug($"SQLiteExec: {sqlResult}"); };
+
+            return factory.FromQuery(query);
+        }
+    }
+}
