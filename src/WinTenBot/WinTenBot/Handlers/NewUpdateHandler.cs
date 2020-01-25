43b0ce96d6552cd3e561531aad06294e09259dba
Azhe Kun <azhe403@gmail.com>
2020-01-25 19:57:26 +0700

    more pararelize on update and code cleanup

--- src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
@@ -3,168 +3,49 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Serilog;
-using SqlKata;
-using SqlKata.Execution;
 using Telegram.Bot.Framework.Abstractions;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.ReplyMarkups;
 using WinTenBot.Helpers;
 using WinTenBot.Providers;
-using WinTenBot.Services;
 
 namespace WinTenBot.Handlers
 {
     public class NewUpdateHandler : IUpdateHandler
     {
-        private readonly AfkService _afkService;
-        private NotesService _notesService;
         private RequestProvider _requestProvider;
 
         public NewUpdateHandler()
         {
-            _afkService = new AfkService();
-            _notesService = new NotesService();
+           
         }
 
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
             _requestProvider = new RequestProvider(context);
             var message = context.Update.Message ?? context.Update.CallbackQuery.Message;
-            
+
             Log.Information("New Update");
 
-            Parallel.Invoke(
-                async () => await _requestProvider.AfkCheck(message), 
-                async () => await _requestProvider.CheckUsername(message),
-                async () => await _requestProvider.FindNotesAsync(message),
-                async () => await ActivityHelper.HitActivity(message));
+            var actions = new List<Action>();
+            
+            actions.Add(async () => await _requestProvider.AfkCheck(message));
+            actions.Add(async () => await _requestProvider.CheckCasBanAsync(message.From));
+            actions.Add(async () => await _requestProvider.CheckUsername(message));
+            actions.Add(async () => await _requestProvider.FindNotesAsync(message));
+            actions.Add(async () => await ActivityHelper.HitActivity(message));
 
             if (context.Update.CallbackQuery == null)
             {
-                Parallel.Invoke(
-                    async () => await _requestProvider.CheckMessage(message));
+                actions.Add(async () => await _requestProvider.CheckMessage(message));
             }
 
             if (!_requestProvider.IsPrivateChat())
             {
-                Parallel.Invoke(async () => await _requestProvider.CheckGlobalBanAsync(message));
+                actions.Add(async () => await _requestProvider.CheckGlobalBanAsync(message));
             }
+            
+            Parallel.Invoke(actions.ToArray());
 
             await next(context, cancellationToken);
-        }
-
-        private async Task AfkCheck(Message message)
-        {
-            if (message.ReplyToMessage != null)
-            {
-                var repMsg = message.ReplyToMessage;
-                var isAfkReply = await _afkService.IsAfkAsync(repMsg);
-                if (isAfkReply)
-                    await $"{repMsg.GetFromNameLink()} sedang afk".SendTextAsync();
-            }
-
-            var isAfk = await _afkService.IsAfkAsync(message);
-            if (isAfk)
-            {
-                await $"{message.GetFromNameLink()} sudah tidak afk".SendTextAsync();
-
-                var data = new Dictionary<string, object>()
-                {
-                    {"chat_id", message.Chat.Id},
-                    {"user_id", message.From.Id},
-                    {"is_afk", 0},
-                    {"afk_reason", ""}
-                };
-
-                await _afkService.SaveAsync(data);
-                await _afkService.UpdateCacheAsync();
-            }
-        }
-
-        private async Task CheckGlobalBanAsync(Message message)
-        {
-            var userId = message.From.Id;
-            var user = message.From;
-            var messageId = message.MessageId;
-
-            // var isBan = await _elasticSecurityService.IsExist(userId);
-            var isBan = await user.IsBanInCache();
-            ConsoleHelper.WriteLine($"IsBan: {isBan}");
-            if (isBan)
-            {
-                await _requestProvider.DeleteAsync(messageId);
-                await _requestProvider.KickMemberAsync(user);
-                await _requestProvider.UnbanMemberAsync(user);
-            }
-        }
-
-        private async Task HitActivity(Message message)
-        {
-            var data = new Dictionary<string, object>()
-            {
-                {"via_bot", "ZiziBeta"},
-                {"message_type", message.Type.ToString()},
-                {"from_id", message.From.Id},
-                {"from_first_name", message.From.FirstName},
-                {"from_last_name", message.From.LastName},
-                {"from_username", message.From.Username},
-                {"from_lang_code", message.From.LanguageCode},
-                {"chat_id", message.Chat.Id},
-                {"chat_username", message.Chat.Username},
-                {"chat_type", message.Chat.Type.ToString()},
-                {"chat_title", message.Chat.Title},
-            };
-
-            var insertHit = await new Query("hit_activity")
-                .ExecForMysql()
-                .InsertAsync(data);
-            ConsoleHelper.WriteLine($"Insert Hit: {insertHit}");
-        }
-
-        private async Task FindNotesAsync(Message msg)
-        {
-            InlineKeyboardMarkup _replyMarkup = null;
-            try
-            {
-                var selectedNotes = await _notesService.GetNotesBySlug(msg.Chat.Id, msg.Text);
-                if (selectedNotes.Count > 0)
-                {
-                    var content = selectedNotes[0].Content;
-                    var btnData = selectedNotes[0].BtnData;
-                    if (btnData != "")
-                    {
-                        _replyMarkup = btnData.ToReplyMarkup(2);
-                    }
-
-                    await _requestProvider.SendTextAsync(content, _replyMarkup);
-                    _replyMarkup = null;
-
-                    foreach (var note in selectedNotes)
-                    {
-                        Log.Debug(note.ToJson());
-                    }
-                }
-                else
-                {
-                    Log.Debug("No rows result set in Notes");
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex,"Error when getting Notes");
-            }
-        }
-
-        private async Task CheckMessage(Message message)
-        {
-            var text = message.Text;
-            if (text != null)
-            {
-                var isMustDelete = await MessageHelper.IsMustDelete(text);
-                Log.Debug($"Message {message.MessageId} IsMustDelete: {isMustDelete}");
-
-                if (isMustDelete) await _requestProvider.DeleteAsync(message.MessageId);
-            }
         }
     }
 }
