67852eb7ade4e323da6e3859f8b1b5a3765d349c
Azhe Kun <azhe403@gmail.com>
2020-01-25 15:03:51 +0700

    GenericMessageHandler no longer used

--- src/WinTenBot/WinTenBot/Handlers/GenericMessageHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/GenericMessageHandler.cs
@@ -1,13 +1,8 @@
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
 using Telegram.Bot.Framework.Abstractions;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
-using WinTenBot.Helpers;
 using WinTenBot.Helpers.Processors;
-using WinTenBot.Model;
 using WinTenBot.Providers;
 using WinTenBot.Services;
 
@@ -15,42 +10,42 @@ namespace WinTenBot.Handlers
 {
     public class GenericMessageHandler : IUpdateHandler
     {
-        private CasBanProvider _casBanProvider;
-        private ChatProcessor _chatProcessor;
-        private NotesService _notesService;
-        private IReplyMarkup _replyMarkup;
-        private AfkService _afkService;
-        private ElasticSecurityService _elasticSecurityService;
+        // private CasBanProvider _casBanProvider;
+        // private ChatProcessor _chatProcessor;
+        // private NotesService _notesService;
+        // private IReplyMarkup _replyMarkup;
+        // private AfkService _afkService;
+        // private ElasticSecurityService _elasticSecurityService;
 
         public GenericMessageHandler()
         {
-            _casBanProvider = new CasBanProvider();
-            _notesService = new NotesService();
-            _afkService = new AfkService();
-            _replyMarkup = null;
+            // _casBanProvider = new CasBanProvider();
+            // _notesService = new NotesService();
+            // _afkService = new AfkService();
+            // _replyMarkup = null;
         }
 
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
-            _chatProcessor = new ChatProcessor(context);
-            _elasticSecurityService = new ElasticSecurityService(context.Update.Message);
+            // _chatProcessor = new ChatProcessor(context);
+            // _elasticSecurityService = new ElasticSecurityService(context.Update.Message);
             
-            Message msg = context.Update.Message;
+            // Message msg = context.Update.Message;
 
-            ConsoleHelper.WriteLine(msg.ToJson());
-            ConsoleHelper.WriteLine(msg.Text);
+            // ConsoleHelper.WriteLine(msg.ToJson());
+            // ConsoleHelper.WriteLine(msg.Text);
 
 
-            if (msg.Text == "ping")
-            {
+            // if (msg.Text == "ping")
+            // {
                 // run /ping?
                 
-            }
+            // }
             
-            if (Bot.HostingEnvironment.IsProduction())
-            {
-                await _casBanProvider.IsCasBan(msg.From.Id);
-            }
+            // if (Bot.HostingEnvironment.IsProduction())
+            // {
+            //     await _casBanProvider.IsCasBan(msg.From.Id);
+            // }
 
             // Parallel.Invoke(async () => await CheckMessage(msg));
             
@@ -86,58 +81,6 @@ namespace WinTenBot.Handlers
             // CheckUsername(msg);
 
             await next(context);
-        }
-
-        private async Task AfkCheck(Message message)
-        {
-            if (message.ReplyToMessage != null)
-            {
-                var repMsg = message.ReplyToMessage;
-                var isAfkReply = await _afkService.IsAfkAsync(repMsg);
-                if (isAfkReply)
-                    await _chatProcessor.SendAsync($"{repMsg.GetFromNameLink()} sedang afk");
-            }
-            
-            var isAfk = await _afkService.IsAfkAsync(message);
-            if (isAfk)
-            {
-                await _chatProcessor.SendAsync($"{message.GetFromNameLink()} sudah tidak afk");
-                
-                var data = new Dictionary<string, object>()
-                {
-                    {"chat_id",message.Chat.Id},
-                    {"user_id", message.From.Id},
-                    {"is_afk", 0},
-                    {"afk_reason",""}
-                };
-
-                await _afkService.SaveAsync(data);
-                await _afkService.UpdateCacheAsync();
-            }
-        }
-
-        private async Task CheckGlobalBanAsync(Message message)
-        {
-            var userId = message.From.Id;
-            var user = message.From;
-            var messageId = message.MessageId;
-            
-            // var isBan = await _elasticSecurityService.IsExist(userId);
-            var isBan = await user.IsBanInCache();
-            ConsoleHelper.WriteLine($"IsBan: {isBan}");
-            if (isBan)
-            {
-                await _chatProcessor.DeleteAsync(messageId);
-                await _chatProcessor.KickMemberAsync(user);
-                await _chatProcessor.UnbanMemberAsync(user);
-            }
-        }
-
-        private void CheckUsername(Message message)
-        {
-            var fromUser = message.From;
-            var noUsername = fromUser.IsNoUsername();
-            ConsoleHelper.WriteLine($"{fromUser} IsNoUsername: {noUsername}");
         }
     }
 }
