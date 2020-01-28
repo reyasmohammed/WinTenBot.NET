b575bc14ade9e80a64eb4f8e9b0bb897e3b20ad2
Azhe-kun <azhe403@gmail.com>
2020-01-28 21:32:37 +0700

    hit activity in background

--- src/WinTenBot/WinTenBot/Helpers/ActivityHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/ActivityHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Hangfire;
+using Serilog;
 using SqlKata;
 using SqlKata.Execution;
 using Telegram.Bot.Types;
@@ -11,26 +13,35 @@ namespace WinTenBot.Helpers
     {
         public static async Task HitActivity(Message message)
         {
-            var data = new Dictionary<string, object>()
-            {
-                {"via_bot", "ZiziBeta"},
-                {"message_type", message.Type.ToString()},
-                {"from_id", message.From.Id},
-                {"from_first_name", message.From.FirstName},
-                {"from_last_name", message.From.LastName},
-                {"from_username", message.From.Username},
-                {"from_lang_code", message.From.LanguageCode},
-                {"chat_id", message.Chat.Id},
-                {"chat_username", message.Chat.Username},
-                {"chat_type", message.Chat.Type.ToString()},
-                {"chat_title", message.Chat.Title},
-            };
-
-            var insertHit = await new Query("hit_activity")
-                .ExecForMysql()
-                .InsertAsync(data);
             
-            ConsoleHelper.WriteLine($"Insert Hit: {insertHit}");
+                var data = new Dictionary<string, object>()
+                {
+                    {"via_bot", "ZiziBeta"},
+                    {"message_type", message.Type.ToString()},
+                    {"from_id", message.From.Id},
+                    {"from_first_name", message.From.FirstName},
+                    {"from_last_name", message.From.LastName},
+                    {"from_username", message.From.Username},
+                    {"from_lang_code", message.From.LanguageCode},
+                    {"chat_id", message.Chat.Id},
+                    {"chat_username", message.Chat.Username},
+                    {"chat_type", message.Chat.Type.ToString()},
+                    {"chat_title", message.Chat.Title},
+                };
+
+                var insertHit = await new Query("hit_activity")
+                    .ExecForMysql()
+                    .InsertAsync(data);
+
+                ConsoleHelper.WriteLine($"Insert Hit: {insertHit}");
+            
+        }
+
+        public static void HitActivityBackground(Message message)
+        {
+            BackgroundJob.Enqueue(() => HitActivity(message));
+
+            Log.Information("Hit Activity scheduled in Background");
         }
     }
 }
