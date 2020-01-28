0d94f11fbde98f1357cdfcc1dc43f94d97245053
Azhe-kun <azhe403@gmail.com>
2020-01-28 21:34:03 +0700

    add clearlog helper

--- src/WinTenBot/WinTenBot/Helpers/BotHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/BotHelper.cs
@@ -1,7 +1,12 @@
-﻿using System.Linq;
+﻿using Serilog;
+using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
 using WinTenBot.Model;
+using File = System.IO.File;
 
 namespace WinTenBot.Helpers
 {
@@ -18,6 +23,37 @@ namespace WinTenBot.Helpers
         {
             var me = await Bot.Client.GetMeAsync();
             return (from user in users where user.Id == me.Id select user.Id == me.Id).FirstOrDefault();
+        }
+
+        public static async Task ClearLog()
+        {
+            const string logsPath = "Storage/Logs";
+            var botClient = Bot.Client;
+            var channelTarget = Bot.GlobalConfiguration["CommonConfig:ChannelLogs"].ToInt64();
+
+            var dirInfo = new DirectoryInfo(logsPath);
+            var files = dirInfo.GetFiles();
+            var filteredFiles = files.Where(fileInfo => fileInfo.CreationTimeUtc < DateTime.UtcNow.AddDays(-1)).ToArray();
+
+            if (filteredFiles.Length > 0)
+            {
+                Log.Information($"Found {filteredFiles.Length} of {files.Length}");
+                foreach (var fileInfo in filteredFiles)
+                {
+                    var filePath = fileInfo.FullName;
+                    Log.Information($"Uploading file {filePath}");
+                    var fileStream = File.OpenRead(filePath);
+
+                    var media = new InputOnlineFile(fileStream, fileInfo.Name);
+                    await botClient.SendDocumentAsync(channelTarget, media);
+
+                    filePath.DeleteFile();
+                }
+            }
+            else
+            {
+                Log.Information("No Logs file need be processed for previous date");
+            }
         }
     }
 }
