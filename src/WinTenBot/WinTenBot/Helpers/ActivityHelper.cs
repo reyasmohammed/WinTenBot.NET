7d6c4b8a782101f76d1f8a9a75a7d5a4349ae390
Azhe-kun <azhe403@gmail.com>
2020-02-01 18:11:15 +0700

    via bot now based on GetMe

--- src/WinTenBot/WinTenBot/Helpers/ActivityHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/ActivityHelper.cs
@@ -11,35 +11,36 @@ namespace WinTenBot.Helpers
 {
     public static class ActivityHelper
     {
-        public static async Task HitActivity(Message message)
+        public static async Task HitActivity(this RequestProvider requestProvider)
         {
-            
-                var data = new Dictionary<string, object>()
-                {
-                    {"via_bot", "ZiziBeta"},
-                    {"message_type", message.Type.ToString()},
-                    {"from_id", message.From.Id},
-                    {"from_first_name", message.From.FirstName},
-                    {"from_last_name", message.From.LastName},
-                    {"from_username", message.From.Username},
-                    {"from_lang_code", message.From.LanguageCode},
-                    {"chat_id", message.Chat.Id},
-                    {"chat_username", message.Chat.Username},
-                    {"chat_type", message.Chat.Type.ToString()},
-                    {"chat_title", message.Chat.Title},
-                };
+            var message = requestProvider.Message;
+            var botUser = await requestProvider.GetBotUser();
+            var data = new Dictionary<string, object>()
+            {
+                {"via_bot", botUser.Username},
+                {"message_type", message.Type.ToString()},
+                {"from_id", message.From.Id},
+                {"from_first_name", message.From.FirstName},
+                {"from_last_name", message.From.LastName},
+                {"from_username", message.From.Username},
+                {"from_lang_code", message.From.LanguageCode},
+                {"chat_id", message.Chat.Id},
+                {"chat_username", message.Chat.Username},
+                {"chat_type", message.Chat.Type.ToString()},
+                {"chat_title", message.Chat.Title},
+            };
 
-                var insertHit = await new Query("hit_activity")
-                    .ExecForMysql()
-                    .InsertAsync(data);
+            var insertHit = await new Query("hit_activity")
+                .ExecForMysql()
+                .InsertAsync(data);
 
-                ConsoleHelper.WriteLine($"Insert Hit: {insertHit}");
-            
+            Log.Information($"Insert Hit: {insertHit}");
         }
 
-        public static void HitActivityBackground(Message message)
+        public static void HitActivityBackground(this RequestProvider requestProvider)
         {
-            BackgroundJob.Enqueue(() => HitActivity(message));
+            var message = requestProvider.Message;
+            BackgroundJob.Enqueue(() => HitActivity(requestProvider));
 
             Log.Information("Hit Activity scheduled in Background");
         }
