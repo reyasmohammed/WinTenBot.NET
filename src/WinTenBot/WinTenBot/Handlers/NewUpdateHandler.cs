793c0f17edad6a53e34798039e8727f87f32f9c3
Azhe-kun <azhe403@gmail.com>
2020-02-02 12:02:39 +0700

    use task whenall

--- src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Serilog;
 using Telegram.Bot.Framework.Abstractions;
 using WinTenBot.Helpers;
 using WinTenBot.Providers;
@@ -27,25 +27,37 @@ namespace WinTenBot.Handlers
             Log.Information("New Update");
 
             var actions = new List<Action>();
+            var tasks = new List<Task>();
 
-            actions.Add(async () => await _requestProvider.AfkCheck(message));
-            actions.Add(async () => await _requestProvider.CheckCasBanAsync(message.From));
-            actions.Add(async () => await _requestProvider.CheckUsername(message));
-            actions.Add(async () => await _requestProvider.FindNotesAsync(message));
-            actions.Add(async () => await _requestProvider.HitActivity());
+            tasks.Add(_requestProvider.AfkCheck(message));
+            tasks.Add(_requestProvider.CheckCasBanAsync(message.From));
+            tasks.Add(_requestProvider.CheckUsername(message));
+            tasks.Add(_requestProvider.FindNotesAsync(message));
+            tasks.Add(_requestProvider.HitActivity());
+
+            // actions.Add(async () => await _requestProvider.AfkCheck(message));
+            // actions.Add(async () => await _requestProvider.CheckCasBanAsync(message.From));
+            // actions.Add(async () => await _requestProvider.CheckUsername(message));
+            // actions.Add(async () => await _requestProvider.FindNotesAsync(message));
+            // actions.Add(async () => await _requestProvider.HitActivity());
 
             if (context.Update.CallbackQuery == null)
             {
-                actions.Add(async () => await _requestProvider.CheckMessage(message));
+                // actions.Add(async () => await _requestProvider.CheckMessage(message));
+                tasks.Add(_requestProvider.CheckMessage(message));
             }
 
             if (!_requestProvider.IsPrivateChat())
             {
-                actions.Add(async () => await _requestProvider.EnsureChatRestriction());
-                actions.Add(async () => await _requestProvider.CheckGlobalBanAsync(message));
+                // actions.Add(async () => await _requestProvider.EnsureChatRestriction());
+                // actions.Add(async () => await _requestProvider.CheckGlobalBanAsync(message));
+
+                tasks.Add(_requestProvider.EnsureChatRestriction());
+                tasks.Add(_requestProvider.CheckGlobalBanAsync(message));
             }
 
-            Parallel.Invoke(actions.ToArray());
+            // Parallel.Invoke(actions.ToArray());
+            await Task.WhenAll(tasks);
 
             await next(context, cancellationToken);
         }
