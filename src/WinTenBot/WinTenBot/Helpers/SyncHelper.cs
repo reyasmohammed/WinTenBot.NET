49dfa08df2eddf4ed7044879e99cbd8ed6d871eb
Azhe-kun <azhe403@gmail.com>
2020-02-20 21:47:16 +0700

    add sync helper

---
+++ src/WinTenBot/WinTenBot/Helpers/SyncHelper.cs
@@ -0,0 +1,125 @@
+﻿using Serilog;
+using SqlKata;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinTenBot.Model;
+using WinTenBot.Providers;
+
+namespace WinTenBot.Helpers
+{
+    public static class SyncHelper
+    {
+        public static async Task SyncRssHistoryToCloud()
+        {
+            var prevDate = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+            var queryBase = new Query("rss_history");
+
+            var localHistory = await queryBase
+                .WhereLike("created_at", $"%{prevDate}%")
+                .ExecForSqLite()
+                .GetAsync();
+
+            var mappedHistory = localHistory.ToJson().MapObject<List<RssHistory>>();
+            Log.Information($"RSS History {prevDate} {mappedHistory.Count}");
+
+            Log.Information($"Migrating RSS History to Cloud");
+
+            var valuesInsert = new List<string>();
+            foreach (var history in mappedHistory)
+            {
+                var list1 = new List<object>();
+                // list1.Add("aa","aaa");
+                var values = $"('{history.Url}', '{history.RssSource}', '{history.ChatId}', '{history.Title}', " +
+                             $"'{history.PublishDate}', '{history.Author}', '{history.CreatedAt}')";
+                valuesInsert.Add(values);
+                // var data = new Dictionary<string, object>()
+                // {
+                // {"url", history.Url},
+                // {"rss_source", history.RssSource},
+                // {"chat_id", history.ChatId},
+                // {"title", history.Title},
+                // {"publish_date", history.PublishDate},
+                // {"author", history.Author},
+                // {"created_at", history.CreatedAt}
+                // };
+
+                // await queryBase.ExecForMysql(true).InsertAsync(data);
+            }
+
+            var valuesStr = valuesInsert.MkJoin(", ");
+            Log.Debug($"RssHistory: \n{valuesStr}");
+
+            var sqlInsert = $"INSERT INTO rss_history " +
+                            $"(url, rss_source, chat_id, title, publish_date, author, created_at) " +
+                            $"VALUES {valuesInsert.MkJoin(", ")}";
+
+            await sqlInsert.ExecForMysqlNonQueryAsync(printSql: true);
+
+            // queryBase.ExecForMysql()
+
+            Log.Information($"RSS History migrated.");
+        }
+
+        public static async Task SyncGBanToLocal(bool cleanSync = false)
+        {
+            Log.Information("Getting FBam data..");
+            var cloudQuery = await new Query("fbans")
+                .ExecForMysql()
+                .GetAsync();
+
+            var mappedQuery = cloudQuery.ToJson(followProperty:true).MapObject<List<GlobalBan>>();
+
+            Log.Information($"Gban User: {mappedQuery.Count} rows");
+            
+            var valuesBuilder = new List<string>();
+            foreach (var globalBan in mappedQuery)
+            {
+                var values = new List<string>();
+                values.Add($"'{globalBan.UserId}'");
+                values.Add($"'{globalBan.ReasonBan.SqlEscape().RemoveThisChar("[]'")}'");
+
+                values.Add($"'{globalBan.BannedBy}'");
+                values.Add($"'{globalBan.BannedFrom}'");
+                values.Add($"'{globalBan.CreatedAt}'");
+
+                valuesBuilder.Add($"({values.MkJoin(", ")})");
+            }
+
+            var insertCols = "(user_id,reason_ban,banned_by,banned_from,created_at)";
+            var insertVals = valuesBuilder;
+
+            Log.Information("Values chunk by 1000 rows.");
+            var chunkInsert = insertVals.ChunkBy(1000);
+
+            var step = 1;
+            foreach (var insert in chunkInsert)
+            {
+                var values = insert.MkJoin(",\n");
+                var insertSql = $"INSERT INTO fban_user {insertCols} VALUES \n{values}";
+
+                Log.Information($"Insert part {step++}");
+                await insertSql.ExecForSqLite(true);
+            }
+  
+
+            // foreach (var globalBan in mappedQuery)
+            // {
+            //     var data = new Dictionary<string, object>();
+            //     data.Add("user_id", globalBan.UserId);
+            //     data.Add("reason_ban", globalBan.ReasonBan);
+            //     data.Add("banned_by", globalBan.BannedBy);
+            //     data.Add("banned_from", globalBan.BannedFrom);
+            //     data.Add("created_at", globalBan.CreatedAt);
+            //
+            //     var insert = await new Query("fban_user")
+            //         .ExecForSqLite(true)
+            //         .InsertAsync(data);
+            // }
+
+            await "fban_user".DeleteDuplicateRow("user_id");
+        }
+    }
+}
