c8bdab3b3df923712dbb9f5207ac702810fe7d07
Azhe Kun <azhe403@gmail.com>
2020-02-25 21:33:15 +0700

    moving use sql kata and serilog

--- src/WinTenBot/WinTenBot/Services/ElasticSecurityService.cs
+++ src/WinTenBot/WinTenBot/Services/ElasticSecurityService.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using SqlKata;
+using SqlKata.Execution;
 using Telegram.Bot.Types;
 using WinTenBot.Helpers;
 using WinTenBot.Providers;
 
 namespace WinTenBot.Services
 {
-    public class ElasticSecurityService : Query
+    public class ElasticSecurityService
     {
         private string fbanTable = "fbans";
         private string fileJson = "fban_user.json";
-        private MySqlProvider _mySqlProvider;
+        // private MySqlProvider _mySqlProvider;
         private readonly Message _message;
 
         public ElasticSecurityService(Message message)
         {
             _message = message;
-            _mySqlProvider = new MySqlProvider();
+            // _mySqlProvider = new MySqlProvider();
         }
 
         public async Task<bool> IsExist(int userId)
@@ -28,7 +30,14 @@ namespace WinTenBot.Services
             {
                 {"user_id", userId}
             };
-            return await IsDataExist(fbanTable, where);
+            // return await IsDataExist(fbanTable, where);
+            
+            var query = await new Query(fbanTable)
+                .ExecForMysql(true)
+                .Where(where)
+                .GetAsync();
+
+            return query.Any();
         }
 
         public async Task<bool> IsExistInCache(int userId)
@@ -36,7 +45,7 @@ namespace WinTenBot.Services
             var data = await ReadCacheAsync();
             DataTable filtered = new DataTable(null);
             
-            ConsoleHelper.WriteLine($"Checking {userId} in Global Ban Cache");
+            Log.Information($"Checking {userId} in Global Ban Cache");
             var search = data.AsEnumerable()
                 .Where(row => row.Field<string>("user_id") == userId.ToString());
             if (search.Any())
@@ -44,26 +53,42 @@ namespace WinTenBot.Services
                 filtered = search.CopyToDataTable();
             }
 
-            ConsoleHelper.WriteLine($"Caches found: {filtered.ToJson()}");
+            Log.Information($"Caches found: {filtered.ToJson()}");
             return filtered.Rows.Count > 0;
         }
 
         public async Task<bool> SaveBanAsync(Dictionary<string, object> data)
         {
-            ConsoleHelper.WriteLine($"Inserting new data for {_message.Chat.Id}");
-            return await Insert(fbanTable, data);
+            Log.Information($"Inserting new data for {_message.Chat.Id}");
+            var query = await new Query(fbanTable)
+                .ExecForMysql(true)
+                .InsertAsync(data);
+
+            return query > 0;
+            // return await Insert(fbanTable, data);
         }
 
         public async Task<bool> DeleteBanAsync(int userId)
         {
             var where = new Dictionary<string, object>() {{"user_id",userId}};
-            return await Delete(fbanTable, where);
+            var delete = await new Query(fbanTable)
+                .ExecForMysql(true)
+                .Where(where)
+                .DeleteAsync();
+
+            return delete > 0;
+            // return await Delete(fbanTable, where);
         }
 
         public async Task<DataTable> GetGlobalBanAll()
         {
-            var sql = $"SELECT * FROM {fbanTable}";
-            var data = await _mySqlProvider.ExecQueryAsync(sql);
+            var query = await new Query(fbanTable)
+                .ExecForMysql(true)
+                .GetAsync();
+
+            var data = query.ToJson().MapObject<DataTable>();
+            // var sql = $"SELECT * FROM {fbanTable}";
+            // var data = await _mySqlProvider.ExecQueryAsync(sql);
             return data;
         }
 
@@ -71,7 +96,7 @@ namespace WinTenBot.Services
         {
             var data = await GetGlobalBanAll();
 
-            ConsoleHelper.WriteLine($"Updating Global Ban caches to {fileJson}");
+            Log.Information($"Updating Global Ban caches to {fileJson}");
             await data.WriteCacheAsync(fileJson);
         }
 
--- src/WinTenBot/WinTenBot/Services/MediaFilterService.cs
+++ src/WinTenBot/WinTenBot/Services/MediaFilterService.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.IO;
-using System.Threading.Tasks;
-using WinTenBot.Helpers;
 using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+using SqlKata;
+using SqlKata.Execution;
+using WinTenBot.Helpers;
 using WinTenBot.Providers;
 
 namespace WinTenBot.Services
 {
     public class MediaFilterService
     {
-        private MySqlProvider _mySqlProvider;
         private string baseTable = "media_filters";
         private string fileJson = "media_filter.json";
 
 
-        public MediaFilterService()
-        {
-            _mySqlProvider = new MySqlProvider();
-        }
-
         public async Task<bool> IsExist(string key, string value)
         {
-            var sql = $"SELECT * FROM {baseTable} WHERE {key} = '{value}'";
-            var data = await _mySqlProvider.ExecQueryAsync(sql);
-            return data.Rows.Count > 0;
+            var query = await new Query(baseTable)
+                .ExecForMysql()
+                .Where(key, value)
+                .GetAsync();
+            return query.Any();
+
+            // var sql = $"SELECT * FROM {baseTable} WHERE {key} = '{value}'";
+            // var data = await _mySqlProvider.ExecQueryAsync(sql);
+            // return data.Rows.Count > 0;
         }
 
         public async Task<bool> IsExistInCache(string key, string val)
@@ -36,30 +37,37 @@ namespace WinTenBot.Services
             if (!search.Any()) return false;
 
             var filtered = search.CopyToDataTable();
-            ConsoleHelper.WriteLine($"Media found in Caches: {filtered.ToJson()}");
+            Log.Information($"Media found in Caches: {filtered.ToJson()}");
             return true;
         }
 
         public async Task SaveAsync(Dictionary<string, object> data)
         {
 //            var json = TextHelper.ToJson(data);
-            ConsoleHelper.WriteLine(data.ToJson());
-
-            var insert = await _mySqlProvider.Insert(baseTable, data);
-            ConsoleHelper.WriteLine($"SaveFile: {insert}");
+            Log.Information(data.ToJson());
+            var insert = await new Query(baseTable)
+                .ExecForMysql()
+                .InsertAsync(data);
+            
+            // var insert = await _mySqlProvider.Insert(baseTable, data);
+            Log.Information($"SaveFile: {insert}");
         }
 
         public async Task<DataTable> GetAllMedia()
         {
-            var sql = $"SELECT * FROM {baseTable}";
-            var data = await _mySqlProvider.ExecQueryAsync(sql);
+            var query = await new Query(baseTable)
+                .ExecForMysql(true)
+                .GetAsync();
+            // var sql = $"SELECT * FROM {baseTable}";
+            // var data = await _mySqlProvider.ExecQueryAsync(sql);
+            var data = query.ToJson().MapObject<DataTable>();
             return data;
         }
 
         public async Task UpdateCacheAsync()
         {
-            var data = await GetAllMedia();
-            ConsoleHelper.WriteLine($"Updating Media Filter caches to {fileJson}");
+            var data = await GetAllMedia(); 
+            Log.Information($"Updating Media Filter caches to {fileJson}");
 
             await data.WriteCacheAsync(fileJson);
         }
