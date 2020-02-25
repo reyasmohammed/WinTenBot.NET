59cdd76bee36b529dc99b14e4733cf33fd41f103
Azhe Kun <azhe403@gmail.com>
2020-02-25 08:12:13 +0700

    add antispam provider. include ES2, SpamWatch API and CAS Ban.

---
+++ src/WinTenBot/WinTenBot/Providers/AntiSpamProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using Serilog;
+using SqlKata;
+using SqlKata.Execution;
+using Telegram.Bot.Types;
+using WinTenBot.Helpers;
+using WinTenBot.Model;
+
+namespace WinTenBot.Providers
+{
+    public static class AntiSpamProvider
+    {
+        public static async Task<SpamWatch> CheckSpamWatch(this int userId)
+        {
+            var spamWatch  = new SpamWatch();
+            var spamWatchToken = Bot.GlobalConfiguration["CommonConfig:SpamWatchToken"];
+            
+            try
+            {
+                var baseUrl = $"https://api.spamwat.ch/banlist/{userId}";
+                spamWatch = await baseUrl
+                    .WithOAuthBearerToken(spamWatchToken)
+                    .GetJsonAsync<SpamWatch>();
+                spamWatch.IsBan = spamWatch.Code != 404;
+                Log.Debug(spamWatch.ToJson(true));
+            }
+            catch (FlurlHttpException ex)
+            {
+                var callHttpStatus = ex.Call.HttpStatus;
+                if (callHttpStatus == HttpStatusCode.NotFound)
+                {
+                    spamWatch.IsBan = false;
+                }
+
+                Log.Information($"StatusCode: {callHttpStatus}");
+                Log.Error(ex, "SpamWatch API FlurlHttpEx");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,"SpamWatch Exception");
+            }
+
+            return spamWatch;
+        }
+        
+        public static async Task<bool> CheckGBan(this int userId)
+        {
+            var query = await new Query("fban_user")
+                .Where("user_id",userId)
+                .ExecForSqLite(true)
+                .GetAsync();
+
+            return query.Any();
+        }
+        
+        public static async Task<bool> IsCasBanAsync(this User user)
+        {
+            var userId = user.Id;
+            var url = "https://api.cas.chat/check".SetQueryParam("user_id", userId);
+            var resp = await url.GetJsonAsync<CasBan>();
+            
+            Log.Debug("CasBan Response", resp);
+
+            var isBan = resp.Ok;
+            Log.Information($"UserId: {userId} is CAS ban: {isBan}");
+            return isBan;
+
+        }
+    }
+}
