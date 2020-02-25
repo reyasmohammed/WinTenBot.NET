46249ca3924b24835c8b165b6fd850327f355194
Azhe Kun <azhe403@gmail.com>
2020-02-25 08:22:12 +0700

    split which should await and skip await task on update

--- src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/NewUpdateHandler.cs
@@ -23,17 +23,20 @@ namespace WinTenBot.Handlers
             if (context.Update.ChannelPost != null) return;
 
             var message = context.Update.Message ?? context.Update.CallbackQuery.Message;
+            var fromUser = message.From;
 
-            Log.Information("New Update");
+            Log.Information($"New Update: {context.Update.ToJson(true)}");
 
-            var actions = new List<Action>();
-            var tasks = new List<Task>();
+            // var actions = new List<Action>();
+            var shouldAwaitTasks = new List<Task>();
+            var nonAwaitTasks = new List<Task>();
 
-            tasks.Add(_requestProvider.AfkCheck(message));
-            tasks.Add(_requestProvider.CheckCasBanAsync(message.From));
-            tasks.Add(_requestProvider.CheckUsername(message));
-            tasks.Add(_requestProvider.FindNotesAsync(message));
-            tasks.Add(_requestProvider.HitActivity());
+            shouldAwaitTasks.Add(_requestProvider.CheckCasBanAsync(fromUser));
+            shouldAwaitTasks.Add(_requestProvider.CheckUsername(message));
+            
+            nonAwaitTasks.Add(_requestProvider.AfkCheck(message));
+            nonAwaitTasks.Add(_requestProvider.FindNotesAsync(message));
+            nonAwaitTasks.Add(_requestProvider.HitActivity());
 
             // actions.Add(async () => await _requestProvider.AfkCheck(message));
             // actions.Add(async () => await _requestProvider.CheckCasBanAsync(message.From));
@@ -44,7 +47,7 @@ namespace WinTenBot.Handlers
             if (context.Update.CallbackQuery == null)
             {
                 // actions.Add(async () => await _requestProvider.CheckMessage(message));
-                tasks.Add(_requestProvider.CheckMessage(message));
+                shouldAwaitTasks.Add(_requestProvider.CheckMessage(message));
             }
 
             if (!_requestProvider.IsPrivateChat())
@@ -52,12 +55,17 @@ namespace WinTenBot.Handlers
                 // actions.Add(async () => await _requestProvider.EnsureChatRestriction());
                 // actions.Add(async () => await _requestProvider.CheckGlobalBanAsync(message));
 
-                tasks.Add(_requestProvider.EnsureChatRestriction());
-                tasks.Add(_requestProvider.CheckGlobalBanAsync(message));
+                shouldAwaitTasks.Add(_requestProvider.EnsureChatRestriction());
+                shouldAwaitTasks.Add(_requestProvider.CheckGlobalBanAsync(message));
             }
 
             // Parallel.Invoke(actions.ToArray());
-            await Task.WhenAll(tasks);
+            await Task.WhenAll(shouldAwaitTasks);
+            
+#pragma warning disable 4014
+            // This List Task should not await.
+            Task.WhenAll(nonAwaitTasks);
+#pragma warning restore 4014
 
             await next(context, cancellationToken);
         }
