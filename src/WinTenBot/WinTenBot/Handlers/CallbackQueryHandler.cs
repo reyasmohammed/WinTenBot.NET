23729c8de32a16787d010c36425e42dc9b07b503
Azhe Kun <azhe403@gmail.com>
2020-02-25 21:41:37 +0700

    use serilog and import clean up

--- src/WinTenBot/WinTenBot/Handlers/CallbackQueryHandler.cs
+++ src/WinTenBot/WinTenBot/Handlers/CallbackQueryHandler.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using LiteDB;
+using Serilog;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types;
 using WinTenBot.Handlers.Callbacks;
 using WinTenBot.Helpers;
-using WinTenBot.Helpers.Processors;
 using WinTenBot.Providers;
 
 namespace WinTenBot.Handlers
@@ -22,20 +19,20 @@ namespace WinTenBot.Handlers
             CallbackQuery cq = context.Update.CallbackQuery;
             _requestProvider.CallBackMessageId = cq.Message.MessageId;
 
-            ConsoleHelper.WriteLine(cq.ToJson());
-            ConsoleHelper.WriteLine($"CallBackData: {cq.Data}");
+            Log.Information(cq.ToJson());
+            Log.Information($"CallBackData: {cq.Data}");
 
             var partsCallback = cq.Data.SplitText(" ");
-            ConsoleHelper.WriteLine($"Callbacks: {partsCallback.ToJson()}");
+            Log.Information($"Callbacks: {partsCallback.ToJson()}");
 
             switch (partsCallback[0]) // Level 0
             {
                 case "help":
                     var sendText = await partsCallback[1].LoadInBotDocs();
-                    ConsoleHelper.WriteLine($"Docs: {sendText}");
+                    Log.Information($"Docs: {sendText}");
                     var subPartsCallback = partsCallback[1].SplitText("/");
 
-                    ConsoleHelper.WriteLine($"SubParts: {subPartsCallback.ToJson()}");
+                    Log.Information($"SubParts: {subPartsCallback.ToJson()}");
                     var jsonButton = partsCallback[1];
 
                     if (subPartsCallback.Count > 1)
