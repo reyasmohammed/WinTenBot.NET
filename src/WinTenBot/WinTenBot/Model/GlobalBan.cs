19f17dbaf2d25910fbc7d7395ab3537790c51ff6
Azhe Kun <azhe403@gmail.com>
2020-02-25 08:18:20 +0700

    add globalban model

---
+++ src/WinTenBot/WinTenBot/Model/GlobalBan.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+
+namespace WinTenBot.Model
+{
+    public class GlobalBan
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("reason_ban")]
+        public string ReasonBan { get; set; }
+
+        [JsonPropertyName("banned_by")]
+        public string BannedBy { get; set; }
+
+        [JsonPropertyName("banned_from")]
+        public string BannedFrom { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+
+    }
+
+}
