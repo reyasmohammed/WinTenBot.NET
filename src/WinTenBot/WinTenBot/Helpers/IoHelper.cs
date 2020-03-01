dac63cee591206ebd400a6cc4fe5d8453b114274
azhe403 <azhe403@gmail.com>
2020-03-01 19:53:29 +0700

    add write text and ensure directory in io helper

--- src/WinTenBot/WinTenBot/Helpers/IoHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/IoHelper.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Serilog;
 
 namespace WinTenBot.Helpers
 {
     public static class IoHelper
     {
-        public static void DeleteFile(this string filePath){
+        public static string BaseDirectory { get; set; } = "Storage/Caches";
+
+        public static void DeleteFile(this string filePath)
+        {
             if (!File.Exists(filePath)) return;
+
             try
             {
                 Log.Information($"Deleting {filePath}");
@@ -16,8 +21,38 @@ namespace WinTenBot.Helpers
             }
             catch (Exception ex)
             {
-                Log.Error(ex,$"Error Deleting file {filePath}");
+                Log.Error(ex, $"Error Deleting file {filePath}");
             }
+        }
+
+        public static string EnsureDirectory(this string dirPath)
+        {
+            if (!dirPath.IsNullOrEmpty())
+                Directory.CreateDirectory(dirPath);
+
+            return dirPath;
+        }
+
+        public static async Task WriteTextAsync(this string content, string filePath)
+        {
+            filePath = $"{BaseDirectory}/{filePath}";
+            Log.Information($"Writing content to {filePath}");
+
+            Path.GetDirectoryName(filePath).EnsureDirectory();
+
+            await File.WriteAllTextAsync(filePath, content);
+            Log.Information("Writing file success..");
+        }
+
+        public static void WriteText(this string content, string filePath)
+        {
+            filePath = $"{BaseDirectory}/{filePath}";
+            Log.Information($"Writing content to {filePath}");
+
+            Path.GetDirectoryName(filePath).EnsureDirectory();
+
+            File.WriteAllText(filePath, content);
+            Log.Information("Writing file success..");
         }
     }
 }
