f8e1daebea2390b8bcc6628954d862ecf7c2334c
Azhe Kun <azhe403@gmail.com>
2020-03-04 17:49:08 +0700

    add some function

--- src/WinTenBot/WinTenBot/Helpers/PrevilegeHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/PrevilegeHelper.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using WinTenBot.Model;
+using WinTenBot.Providers;
 
 namespace WinTenBot.Helpers
 {
@@ -12,18 +16,51 @@ namespace WinTenBot.Helpers
         {
             bool isSudoer = false;
             var sudoers = Bot.GlobalConfiguration.GetSection("Sudoers").Get<List<string>>();
-//            foreach (var sudoer in sudoers)
-//            {
-//                if (sudoer != userId.ToString()) isSudoer = true;
-//                break;
-//            }
             var match = sudoers.FirstOrDefault(x => x == userId.ToString());
+            
             if (match != null)
             {
                 isSudoer = true;
             }
             Log.Information($"UserId: {userId} IsSudoer: {isSudoer}");
             return  isSudoer;
+        }
+        
+        public static async Task<bool> IsAdminOrPrivateChat(this TelegramProvider telegramProvider)
+        {
+            var isAdmin = await IsAdminGroup(telegramProvider);
+            var isPrivateChat = IsPrivateChat(telegramProvider);
+
+            return isAdmin || isPrivateChat;
+        }
+        
+        public static bool IsPrivateChat(this TelegramProvider telegramProvider)
+        {
+            var messageOrEdited = telegramProvider.MessageOrEdited;
+            return messageOrEdited.Chat.Type == ChatType.Private;
+        }
+        
+        public static async Task<bool> IsAdminGroup(this TelegramProvider telegramProvider)
+        {
+            var message = telegramProvider.MessageOrEdited;
+            var client = telegramProvider.Client;
+            
+            var chatId = message.Chat.Id;
+            var userId = message.From.Id;
+            var isAdmin = false;
+
+            if (IsPrivateChat(telegramProvider)) return false;
+            
+            var admins = await client.GetChatAdministratorsAsync(chatId);
+            foreach (var admin in admins)
+            {
+                if (userId == admin.User.Id)
+                {
+                    isAdmin = true;
+                }
+            }
+
+            return isAdmin;
         }
     }
 }
