a4efcca521c909b5e7de1bb5296e6b3ef999f2f8
Azhe Kun <azhe403@gmail.com>
2020-03-08 18:18:45 +0700

    use newtonsoft json

--- src/WinTenBot/WinTenBot/Model/ChatSetting.cs
+++ src/WinTenBot/WinTenBot/Model/ChatSetting.cs
@@ -1,37 +1,37 @@
-﻿using System.Text.Json.Serialization;
+﻿using Newtonsoft.Json;
 
 namespace WinTenBot.Model
 {
     public class ChatSetting
     {
-        [JsonPropertyName("chat_id")]
+        [JsonProperty("chat_id")]
         public string ChatId { get; set; }
         
-        [JsonPropertyName("member_count")]
+        [JsonProperty("member_count")]
         public long MemberCount { get; set; }
         
-        [JsonPropertyName("welcome_message")]
+        [JsonProperty("welcome_message")]
         public string WelcomeMessage { get; set; }
         
-        [JsonPropertyName("welcome_button")]
+        [JsonProperty("welcome_button")]
         public string WelcomeButton { get; set; }
         
-        [JsonPropertyName("welcome_media")]
+        [JsonProperty("welcome_media")]
         public string WelcomeMedia { get; set; }
         
-        [JsonPropertyName("welcome_media_type")]
+        [JsonProperty("welcome_media_type")]
         public string WelcomeMediaType { get; set; }
         
-        [JsonPropertyName("rules_text")]
+        [JsonProperty("rules_text")]
         public string RulesText { get; set; }
         
-        [JsonPropertyName("last_tags_message_id")]
+        [JsonProperty("last_tags_message_id")]
         public string LastTagsMessageId { get; set; }
         
-        [JsonPropertyName("enable_word_filter_per_group")]
+        [JsonProperty("enable_word_filter_per_group")]
         public bool? EnableWordFilterPerGroup { get; set; }
         
-        [JsonPropertyName("enable_word_filter_group_wide")]
+        [JsonProperty("enable_word_filter_group_wide")]
         public bool? EnableWordFilterGroupWide { get; set; }
     }
 }
