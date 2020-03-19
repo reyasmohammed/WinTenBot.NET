b769abf9da9086b7d645f39edd2e0e83f2357efd
Azhe Kun <azhe403@gmail.com>
2020-03-19 11:25:22 +0700

    add/update some functions

--- src/WinTenBot/WinTenBot/Helpers/CacheHelper.cs
+++ src/WinTenBot/WinTenBot/Helpers/CacheHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Hangfire;
 using Serilog;
@@ -10,12 +11,11 @@ namespace WinTenBot.Helpers
     {
         private static string workingDir = "Storage/Caches";
 
-        public static async Task WriteCacheAsync(this object dataTable, string fileJson, bool indented = true)
+        public static async Task WriteCacheAsync(this object data, string fileJson, bool indented = true)
         {
-            var filePath = $"{workingDir}/{fileJson}";
-            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
-
-            var json = dataTable.ToJson(indented);
+            var filePath = $"{workingDir}/{fileJson}".EnsureDirectory();
+            var json = data.ToJson(indented);
+            
             await json.ToFile(filePath);
             Log.Information("Writing cache success..");
         }
@@ -26,13 +26,36 @@ namespace WinTenBot.Helpers
             Log.Information($"Background Write Cache scheduled with ID: {jobId}");
         }
 
-        public static async Task<DataTable> ReadCacheAsync(this string fileJson)
+        public static async Task<T> ReadCacheAsync<T>(this string fileJson)
         {
             var filePath = $"{workingDir}/{fileJson}";
             var json = await File.ReadAllTextAsync(filePath);
-            var dataTable = json.ToDataTable();
-            Log.Information($"Loaded cache. Rows: {dataTable.Rows.Count} items");
+            var dataTable = json.MapObject<T>();
+            
+            // var dataTable = json.ToDataTable();
+            Log.Information($"Loaded cache items");
             return dataTable;
+        }
+
+        public static bool IsFileCacheExist(this string fileName)
+        {
+            var filePath = $"{workingDir}/{fileName}";
+            var isExist = File.Exists(filePath);
+            Log.Information($"IsCache {fileName} Exist: {isExist}");
+            
+            return isExist;
+        }
+
+        public static void ClearCache(string keyword)
+        {
+            Log.Information($"Deleting caches. Keyword {keyword}");
+            
+            var listFile = Directory.GetFiles(workingDir);
+            var listFiltered = listFile.Where(file => 
+                file.Contains(keyword)
+                );
+            
+            Log.Information($"Found cache target {listFiltered.Count()} of {listFile.Count()}");
         }
     }
 }
