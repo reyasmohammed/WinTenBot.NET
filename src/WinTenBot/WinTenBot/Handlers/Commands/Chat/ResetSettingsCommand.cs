35975e8b982cbc6beb7562b792719b6a146f9183
Azhe-kun <azhe403@gmail.com>
2020-03-22 18:37:18 +0700

    more field for reset settings

--- src/WinTenBot/WinTenBot/Handlers/Commands/Chat/ResetSettingsCommand.cs
+++ src/WinTenBot/WinTenBot/Handlers/Commands/Chat/ResetSettingsCommand.cs
@@ -33,8 +33,20 @@ namespace WinTenBot.Handlers.Commands.Chat
                 {
                     ["chat_id"] = chat.Id,
                     ["chat_title"] = chat.Title,
+                    ["enable_afk_stats"] = 1,
+                    ["enable_anti_malfiles"] = 1,
+                    ["enable_fed_cas_ban"] = 1,
+                    ["enable_fed_es2"] = 1,
+                    ["enable_fed_spamwatch"] = 1,
+                    ["enable_url_filtering"] = 1,
+                    ["enable_human_verification"] = 1,
+                    ["enable_reply_notification"] = 1,
+                    ["enable_warn_username"] = 1,
+                    ["enable_word_filter_group_wide"] = 1,
+                    ["enable_word_filter_per_group"] = 1,
+                    ["enable_welcome_message"] = 1,
                 };
-                
+
                 var update = await _settingsService.SaveSettingsAsync(data);
                 Log.Information($"Result: {update}");
 
