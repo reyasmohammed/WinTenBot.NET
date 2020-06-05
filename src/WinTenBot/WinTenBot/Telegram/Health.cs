a970aca221aea6c9f9ac14bdaa8f30dca6a7d831
Azhe Kun <azhe403@gmail.com>
2020-06-06 00:48:56 +0700

    move clearLogs to io.storage

--- src/WinTenBot/WinTenBot/Telegram/Health.cs
+++ src/WinTenBot/WinTenBot/Telegram/Health.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Serilog;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.InputFiles;
 using WinTenBot.Common;
-using WinTenBot.IO;
 using WinTenBot.Model;
 using WinTenBot.Services;
-using File = System.IO.File;
 
 namespace WinTenBot.Telegram
 {
@@ -96,110 +92,6 @@ namespace WinTenBot.Telegram
             return BotSettings.Clients[name];
         }
         
-        public static async Task ClearLog()
-        {
-            try
-            {
-                const string logsPath = "Storage/Logs";
-                var botClient = BotSettings.Client;
-                var channelTarget = BotSettings.GlobalConfiguration["CommonConfig:ChannelLogs"].ToInt64();
-
-                if (!channelTarget.ToString().StartsWith("-100"))
-                {
-                    Log.Information("Please specify ChannelTarget in appsettings.json");
-                    return;
-                }
-
-                var dirInfo = new DirectoryInfo(logsPath);
-                var files = dirInfo.GetFiles();
-                var filteredFiles = files.Where(fileInfo =>
-                    fileInfo.CreationTimeUtc < DateTime.UtcNow.AddDays(-1)).ToArray();
-
-                if (filteredFiles.Length > 0)
-                {
-                    Log.Information($"Found {filteredFiles.Length} of {files.Length}");
-                    foreach (var fileInfo in filteredFiles)
-                    {
-                        var filePath = fileInfo.FullName;
-                        Log.Information($"Uploading file {filePath}");
-                        await using var fileStream = File.OpenRead(filePath);
-
-                        var media = new InputOnlineFile(fileStream, fileInfo.Name);
-                        await botClient.SendDocumentAsync(channelTarget, media)
-                            .ConfigureAwait(false);
-
-                        fileStream.Close();
-                        await fileStream.DisposeAsync().ConfigureAwait(false);
-                        
-                        filePath.DeleteFile();
-                    }
-                }
-                else
-                {
-                    Log.Information("No Logs file need be processed for previous date");
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Error Send .Log file to ChannelTarget.");
-            }
-        }
-
-        public static async Task ClearLogs(this string logsPath, string filter = "", bool upload = true)
-        {
-            try
-            {
-                Log.Information($"Clearing {logsPath}, filter: {filter}, upload: {upload}");
-                var botClient = BotSettings.Client;
-                var channelTarget = BotSettings.BotChannelLogs;
-
-                if (!channelTarget.ToString().StartsWith("-100"))
-                {
-                    Log.Information("Please specify ChannelTarget in appsettings.json");
-                    return;
-                }
-
-                var dirInfo = new DirectoryInfo(logsPath);
-                var files = dirInfo.GetFiles();
-                var filteredFiles = files.Where(fileInfo =>
-                    fileInfo.CreationTimeUtc < DateTime.UtcNow.AddDays(-1) &&
-                    fileInfo.FullName.Contains(filter)).ToArray();
-
-                if (filteredFiles.Length > 0)
-                {
-                    Log.Information($"Found {filteredFiles.Length} of {files.Length}");
-                    foreach (var fileInfo in filteredFiles)
-                    {
-                        var filePath = fileInfo.FullName;
-                        if (upload)
-                        {
-                            Log.Information($"Uploading file {filePath}");
-                            var fileStream = File.OpenRead(filePath);
-
-                            var media = new InputOnlineFile(fileStream, fileInfo.Name);
-                            await botClient.SendDocumentAsync(channelTarget, media)
-                                .ConfigureAwait(false);
-
-                            fileStream.Close();
-                            await fileStream.DisposeAsync().ConfigureAwait(false);
-                        }
-
-                        var old = filePath + ".old";
-                        File.Move(filePath, old);
-                        old.DeleteFile();
-                    }
-                }
-                else
-                {
-                    Log.Information("No Logs file need be processed for previous date");
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Error Send .Log file to ChannelTarget.");
-            }
-        }
-
         public static async Task EnsureChatHealthAsync(this TelegramService telegramService)
         {
             Log.Information("Ensuring chat health..");
