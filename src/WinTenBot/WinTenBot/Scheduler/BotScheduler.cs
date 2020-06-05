7333a17153ae53e4fc3907480b1f34b4d8b29e28
Azhe Kun <azhe403@gmail.com>
2020-06-06 00:14:48 +0700

    change cleaner

--- src/WinTenBot/WinTenBot/Scheduler/BotScheduler.cs
+++ src/WinTenBot/WinTenBot/Scheduler/BotScheduler.cs
@@ -1,4 +1,5 @@
-﻿using Hangfire;
+﻿using sysIO = System.IO;
+using Hangfire;
 using Serilog;
 using WinTenBot.Telegram;
 using WinTenBot.Tools;
@@ -13,22 +14,22 @@ namespace WinTenBot.Scheduler
             // StartLogglyCleanup();
             StartSyncWordFilter();
             // StartSyncGlobalBanToLocal();
-            
+
             RssScheduler.InitScheduler();
         }
 
         public static void StopScheduler()
         {
-            
         }
 
         private static void StartLogCleanupScheduler()
         {
             var jobId = "logfile-cleanup";
+            var path = sysIO.Path.Combine("Storage", "Logs");
 
             Log.Debug($"Starting cron Log Cleaner with id {jobId}");
             RecurringJob.RemoveIfExists(jobId);
-            RecurringJob.AddOrUpdate(jobId, () => Health.ClearLog(), Cron.Hourly);
+            RecurringJob.AddOrUpdate(jobId, () => path.ClearLogs("Zizi", true), Cron.Hourly);
             RecurringJob.Trigger(jobId);
         }
 
@@ -36,17 +37,17 @@ namespace WinTenBot.Scheduler
         {
             const string jobId = "loggly-cleanup";
             const string storageCaches = "Storage/Caches";
-            
+
             Log.Debug($"Starting cron Loggly Cache Cleaner with id {jobId}");
             RecurringJob.RemoveIfExists(jobId);
-            RecurringJob.AddOrUpdate(jobId, () => storageCaches.ClearLogs("Loggly",false), Cron.Hourly);
+            RecurringJob.AddOrUpdate(jobId, () => storageCaches.ClearLogs("Loggly", false), Cron.Hourly);
             RecurringJob.Trigger(jobId);
         }
 
         private static void StartSyncWordFilter()
         {
             const string jobId = "sync-word-filter";
-            
+
             Log.Debug("Starting cron Sync Word Filter to Local Storage");
             RecurringJob.RemoveIfExists(jobId);
             RecurringJob.AddOrUpdate(jobId, () => Sync.SyncWordToLocalAsync(), Cron.Minutely);
